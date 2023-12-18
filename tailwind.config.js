b84954c7087b9d422f276497cb106cdb259e8615
Iqbal LX <iqbal@alumni.pcr.ac.id>
2023-12-18 09:47:58 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {},
   },
   plugins: ['@fortawesome/fontawesome-free/css/all.css'],
